b917d6c5b3eaeff519cdc692bb6e3695686eca74
bung87 <crc32@qq.com>
2026-05-03 09:29:39 +0800

    fix(winapi): add missing VK_OEM_2 constant

--- src/uirelays/drivers/winapi_driver.nim
+++ src/uirelays/drivers/winapi_driver.nim
@@ -167,6 +167,7 @@ const
   VK_F12 = 0x7B'u32
   VK_OEM_COMMA = 0xBC'u32
   VK_OEM_PERIOD = 0xBE'u32
+  VK_OEM_2 = 0xBF'u32
   VK_SHIFT = 0x10'u32
   VK_CONTROL = 0x11'u32
   VK_MENU = 0x12'u32   # Alt
